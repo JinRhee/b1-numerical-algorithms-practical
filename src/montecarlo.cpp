96be2b4b3d188c5cd9ade34bdb9989a3d93bc913
Jin Rhee <JinRhee@users.noreply.github.com>
2024-10-30 15:46:07 +0000

    fixed RV to uniform distribution

--- src/montecarlo.cpp
+++ src/montecarlo.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <random>
 
 /**
- * @brief Calculate pi by monte carlo
+ * @brief Calculate pi by monte carlo with RV following uniform distribution
  * 
  * @param iter_num 
  * @return float 
@@ -9,17 +11,23 @@
 float get_pi(int iter_num = 100) {
     float x, y;
     int total_count = 0, inside_count = 0;
-    std::srand(std::time(nullptr));     // Set rng seed
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    
     for(int i = 0; i < iter_num; i++) {
-        float x = static_cast<float>(rand()/static_cast<float>(RAND_MAX));
-        float y = static_cast<float>(rand()/static_cast<float>(RAND_MAX));
-        if (std::pow(x, 2) + std::pow(y, 2) <= 1.0) inside_count++;
+        if (std::pow(distribution(generator), 2) + std::pow(distribution(generator), 2) <= 1.0) {
+            inside_count++;
+        }
         total_count++;
     }
     return static_cast<float>(inside_count / static_cast<float>(total_count) * 4);
 }
 int main() {
-    float pi = get_pi(100);
-    std::cout << "Pi estimate: " << pi << std::endl;
+    std::ofstream myfile;
+    myfile.open("../file.txt");
+    for(int i = 0; i < 13; i++) {
+        myfile << std::pow(5, i) << ',' << get_pi(std::pow(5, i)) << std::endl;
+    }
+    myfile.close();
     return 0;
 }
