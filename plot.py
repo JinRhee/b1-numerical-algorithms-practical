6d462d31a0abc2873a9e383f502c008b2b7c581d
Jin Rhee <JinRhee@users.noreply.github.com>
2024-11-04 16:07:29 +0000

    add newton raphson plot

--- plot.py
+++ plot.py
@@ -23,20 +23,33 @@ def plot_monte_carlo():
     ax[1].plot(data[0], rel_err, label='Relative error: (estimate - π) / π')
     ax[1].plot(data[0], c*np.reciprocal((np.sqrt(data[0]))), '--g', label='1/sqrt(N)')
     ax[1].legend()
+    ax[1].set_xlabel('Number of samples (N)')
     
-    fig.savefig("plot")
+    fig.savefig("./report/images/monte_carlo_plot")
     plt.show()
 
 def plot_newton_raphson():
+    pi = 3.1415926535897932
     data = np.genfromtxt(fname = "./results/newton_raphson.txt", dtype = float, delimiter = ',').transpose()
-    fig, ax = plt.subplots()
-    ax.plot(data[2], data[3], label='absolute error')
-    ax.legend()
-    ax.title.set_text('Newton-Raphson method')
-    fig.savefig("plot_newton_raphson")
+    fig, ax = plt.subplots(2)
+    ax[0].title.set_text('Newton-Raphson method')
+    ax[0].set_xscale('log') 
+    ax[0].invert_xaxis()
+    ax[0].plot(data[1], data[0], 'b-', label='estimate of π')
+    ax[0].plot(data[1], data[0], 'bo')
+    ax[0].plot(data[1], np.ones_like(data[1])*math.pi, '-r', alpha=0.3, label='π from math.pi')
+    ax[0].legend()
+
+    ax[1].set_xscale('log')
+    ax[1].invert_xaxis()
+    ax[1].plot(data[1], data[3], label='absolute error')
+    ax[1].legend()
+    ax[1].set_xlabel('Minimum error (epsilon) for convergence')
+    
+    fig.savefig("./report/images/plot_newton_raphson")
     plt.show()
 
 if __name__ == "__main__":
-    #plot_monte_carlo()
+    plot_monte_carlo()
     plot_newton_raphson()
     
